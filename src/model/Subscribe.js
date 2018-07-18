b2a6295cbdd8b7c0d42620b9037eeb01c244257e
Oleg <oleg.pav.m@gmail.com>
2018-07-18 13:10:44 +0300

    Subscribe model.

---
+++ src/model/Subscribe.js
@@ -0,0 +1,14 @@
+const core = require('griboyedov');
+const MongoDB = core.service.MongoDB;
+
+module.exports = MongoDB.makeModel('Subscribe', {
+    user: {
+        type: String,
+        required: true,
+        index: true,
+    },
+    key: {
+        type: String,
+        required: true,
+    },
+});
