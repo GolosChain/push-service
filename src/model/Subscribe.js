d81be66e7ea029a979ac96a501d1a194b6f3eda5
Oleg <oleg.pav.m@gmail.com>
2018-07-25 11:28:15 +0300

    Push model schema.

--- src/model/Subscribe.js
+++ src/model/Subscribe.js
@@ -1,18 +1,93 @@
 const core = require('griboyedov');
 const MongoDB = core.service.MongoDB;
 
-module.exports = MongoDB.makeModel('Subscribe', {
-    user: {
-        type: String,
-        required: true,
-        index: true,
+module.exports = MongoDB.makeModel(
+    'Subscribe',
+    {
+        user: {
+            type: String,
+            required: true,
+            index: true,
+        },
+        keys: {
+            type: String,
+            required: true,
+        },
+        deviceType: {
+            type: String,
+            enum: ['ios', 'android', 'web'],
+            required: true,
+        },
+        options: {
+            vote: {
+                type: Boolean,
+                default: true,
+            },
+            flag: {
+                type: Boolean,
+                default: true,
+            },
+            transfer: {
+                type: Boolean,
+                default: true,
+            },
+            reply: {
+                type: Boolean,
+                default: true,
+            },
+            subscribe: {
+                type: Boolean,
+                default: true,
+            },
+            unsubscribe: {
+                type: Boolean,
+                default: true,
+            },
+            mention: {
+                type: Boolean,
+                default: true,
+            },
+            repost: {
+                type: Boolean,
+                default: true,
+            },
+            award: {
+                type: Boolean,
+                default: true,
+            },
+            curatorAward: {
+                type: Boolean,
+                default: true,
+            },
+            message: {
+                type: Boolean,
+                default: true,
+            },
+            witnessVote: {
+                type: Boolean,
+                default: true,
+            },
+            witnessCancelVote: {
+                type: Boolean,
+                default: true,
+            },
+        },
     },
-    key: {
-        type: String,
-        required: true,
-    },
-    deviceType: {
-        type: String,
-        required: true,
-    },
-});
+    {
+        index: [
+            // Basic push broadcast
+            {
+                fields: {
+                    user: 1,
+                },
+            },
+            // Web-push search
+            {
+                fields: {
+                    deviceType: 1,
+                    user: 1,
+                },
+            },
+        ],
+    }
+);
