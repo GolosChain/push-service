e24e940a73f9dcc12198b00e34e424a32c37f93a
Oleg <oleg.pav.m@gmail.com>
2018-09-24 13:41:35 +0300

    #2 Upgrade router-wrapper to connector.

---
+++ src/services/Connector.js
@@ -0,0 +1,25 @@
+const core = require('gls-core-service');
+const BasicConnector = core.services.Connector;
+const Push = require('../controllers/Push');
+const Options = require('../controllers/Options');
+
+class Connector extends BasicConnector {
+    constructor() {
+        super();
+
+        this._push = new Push();
+        this._options = new Options();
+    }
+
+    async start() {
+        await super.start({
+            serverRoutes: {
+                transfer: this._push.broadcast.bind(this._push),
+                getOptions: this._options.get.bind(this._options),
+                setOptions: this._options.set.bind(this._options),
+            },
+        });
+    }
+}
+
+module.exports = Connector;
