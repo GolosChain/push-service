961bdd0d508268e2322bdc7b93ff62a598fcb01f
Oleg <oleg.pav.m@gmail.com>
2018-09-24 13:18:45 +0300

    #3 Remove manual subscribe.

--- src/service/Router.js
+++ src/service/Router.js
@@ -1,7 +1,6 @@
 const core = require('gls-core-service');
 const BasicService = core.service.Basic;
 const Gate = core.service.Gate;
-const RegistratorHandler = require('./handler/Registrator');
 const PushHandler = require('./handler/Push');
 const OptionsHandler = require('./handler/Options');
 
@@ -10,7 +9,6 @@ class Router extends BasicService {
         super();
 
         this._gate = new Gate();
-        this._registrator = new RegistratorHandler();
         this._push = new PushHandler();
         this._options = new OptionsHandler();
     }
@@ -18,7 +16,6 @@ class Router extends BasicService {
     async start() {
         await this._gate.start({
             serverRoutes: {
-                subscribe: this._registrator.register.bind(this._registrator),
                 transfer: this._push.broadcast.bind(this._push),
                 getOptions: this._options.get.bind(this._options),
                 setOptions: this._options.set.bind(this._options),
