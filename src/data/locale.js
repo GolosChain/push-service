77cfe320b33ab84a1c9029c6c2bc4407be577b31
Oleg <oleg.pav.m@gmail.com>
2019-07-23 20:02:12 +0300

    Fix usernames.

--- src/data/locale.js
+++ src/data/locale.js
@@ -6,82 +6,82 @@ module.exports = Template.makeFor({
     event: {
         vote: {
             one: {
-                ru: '${fromUsers[0]} оценил вашу запись. 👍',
-                en: '${fromUsers[0]} liked your entry. 👍',
+                ru: '${actor.username} оценил вашу запись. 👍',
+                en: '${actor.username} liked your entry. 👍',
             },
             many: {
-                ru: '${fromUsers[0]} и еще ${fromUsers.length} оценили вашу запись. 👍',
-                en: '${fromUsers[0]} and ${fromUsers.length} others liked your entry. 👍',
+                ru: '${actor.username} и еще ${fromUsers.length} оценили вашу запись. 👍',
+                en: '${actor.username} and ${fromUsers.length} others liked your entry. 👍',
             },
         },
         flag: {
             one: {
-                ru: '${fromUsers[0]} негативно оценил ваш запись. 😵',
-                en: '${fromUsers[0]} disliked your entry. 😵',
+                ru: '${actor.username} негативно оценил ваш запись. 😵',
+                en: '${actor.username} disliked your entry. 😵',
             },
             many: {
-                ru: '${fromUsers[0]} и еще ${fromUsers.length} негативно оценили вашу запись. 😵',
-                en: '${fromUsers[0]} and ${fromUsers.length} others disliked your entry. 😵',
+                ru: '${actor.username} и еще ${fromUsers.length} негативно оценили вашу запись. 😵',
+                en: '${actor.username} and ${fromUsers.length} others disliked your entry. 😵',
             },
         },
         transfer: {
             one: {
-                ru: '${fromUsers[0]} перевел на ваш счет ${value.amount}. 💸',
-                en: '${fromUsers[0]} has transferred to your account ${value.amount}. 💸',
+                ru: '${actor.username} перевел на ваш счет ${value.amount}. 💸',
+                en: '${actor.username} has transferred to your account ${value.amount}. 💸',
             },
             many: {
-                ru: '${fromUsers[0]} и еще ${fromUsers.length} перевели на ваш счет ${value.amount}. 💸',
-                en: '${fromUsers[0]} and ${fromUsers.length} others were transferred to your ${value.amount} account. 💸',
+                ru: '${actor.username} и еще ${fromUsers.length} перевели на ваш счет ${value.amount}. 💸',
+                en: '${actor.username} and ${fromUsers.length} others were transferred to your ${value.amount} account. 💸',
             },
         },
         reply: {
             one: {
-                ru: '${fromUsers[0]} ответил на вашу запись. ✌️',
-                en: '${fromUsers[0]} replied your entry. ✌️️',
+                ru: '${actor.username} ответил на вашу запись. ✌️',
+                en: '${actor.username} replied your entry. ✌️️',
             },
             many: {
-                ru: '${fromUsers[0]} и еще ${fromUsers.length} ответили вашу запись. ✌️️',
-                en: '${fromUsers[0]} and ${fromUsers.length} others replied your entry. ✌️',
+                ru: '${actor.username} и еще ${fromUsers.length} ответили вашу запись. ✌️️',
+                en: '${actor.username} and ${fromUsers.length} others replied your entry. ✌️',
             },
         },
         subscribe: {
             one: {
-                ru: '${fromUsers[0]} подписался на ваш блог. 😊',
-                en: '${fromUsers[0]} has subscribed to your blog. 😊',
+                ru: '${actor.username} подписался на ваш блог. 😊',
+                en: '${actor.username} has subscribed to your blog. 😊',
             },
             many: {
-                ru: '${fromUsers[0]} и еще ${fromUsers.length} подписались на ваш блог. 😊',
-                en: '${fromUsers[0]} and ${fromUsers.length} others have subscribed to your blog. 😊',
+                ru: '${actor.username} и еще ${fromUsers.length} подписались на ваш блог. 😊',
+                en: '${actor.username} and ${fromUsers.length} others have subscribed to your blog. 😊',
             },
         },
         unsubscribe: {
             one: {
-                ru: '${fromUsers[0]} отписался от вашего блога. 😔',
-                en: '${fromUsers[0]} unsubscribed from your blog. 😔',
+                ru: '${actor.username} отписался от вашего блога. 😔',
+                en: '${actor.username} unsubscribed from your blog. 😔',
             },
             many: {
-                ru: '${fromUsers[0]} и еще ${fromUsers.length} отписались от вашего блога. 😔',
-                en: '${fromUsers[0]} and ${fromUsers.length} others unsubscribed from your blog. 😔',
+                ru: '${actor.username} и еще ${fromUsers.length} отписались от вашего блога. 😔',
+                en: '${actor.username} and ${fromUsers.length} others unsubscribed from your blog. 😔',
             },
         },
         mention: {
             one: {
-                ru: '${fromUsers[0]} упомянув вас в своей записи. 🤔',
-                en: '${fromUsers[0]} mentioned you. 🤔',
+                ru: '${actor.username} упомянув вас в своей записи. 🤔',
+                en: '${actor.username} mentioned you. 🤔',
             },
             many: {
-                ru: '${fromUsers[0]} и еще ${fromUsers.length} упомянули вас. 🤔',
-                en: '${fromUsers[0]} and ${fromUsers.length} others mentioned you. 🤔',
+                ru: '${actor.username} и еще ${fromUsers.length} упомянули вас. 🤔',
+                en: '${actor.username} and ${fromUsers.length} others mentioned you. 🤔',
             },
         },
         repost: {
             one: {
-                ru: '${fromUsers[0]} сделал репост вашего поста. 😎',
-                en: '${fromUsers[0]} made the repost of your post. 😎',
+                ru: '${actor.username} сделал репост вашего поста. 😎',
+                en: '${actor.username} made the repost of your post. 😎',
             },
             many: {
-                ru: '${fromUsers[0]} и еще ${fromUsers.length} сделали репост вашего поста. 😎',
-                en: '${fromUsers[0]} and ${fromUsers.length} others have reposted your post. 😎',
+                ru: '${actor.username} и еще ${fromUsers.length} сделали репост вашего поста. 😎',
+                en: '${actor.username} and ${fromUsers.length} others have reposted your post. 😎',
             },
         },
         reward: {
@@ -106,22 +106,22 @@ module.exports = Template.makeFor({
         },
         witnessVote: {
             one: {
-                ru: '${fromUsers[0]} проголосовал за вас как за делегата. 🔥',
-                en: '${fromUsers[0]} voted for you as a delegate. 🔥',
+                ru: '${actor.username} проголосовал за вас как за делегата. 🔥',
+                en: '${actor.username} voted for you as a delegate. 🔥',
             },
             many: {
-                ru: '${fromUsers[0]} и еще ${fromUsers.length} проголосовали за вас как за делегата. 🔥',
-                en: '${fromUsers[0]} and ${fromUsers.length} others voted for you as delegates. 🔥',
+                ru: '${actor.username} и еще ${fromUsers.length} проголосовали за вас как за делегата. 🔥',
+                en: '${actor.username} and ${fromUsers.length} others voted for you as delegates. 🔥',
             },
         },
         witnessCancelVote: {
             one: {
-                ru: '${fromUsers[0]} снял свой голос с вашей делегатской ноды. 🙈',
-                en: '${fromUsers[0]} has removed your voice from your delegate node. 🙈',
+                ru: '${actor.username} снял свой голос с вашей делегатской ноды. 🙈',
+                en: '${actor.username} has removed your voice from your delegate node. 🙈',
             },
             many: {
-                ru: '${fromUsers[0]} и еще ${fromUsers.length} сняли свои голоса с вашей делегатской ноды. 🙈',
-                en: '${fromUsers[0]} and ${fromUsers.length} others have removed their voices from your delegate node. 🙈',
+                ru: '${actor.username} и еще ${fromUsers.length} сняли свои голоса с вашей делегатской ноды. 🙈',
+                en: '${actor.username} and ${fromUsers.length} others have removed their voices from your delegate node. 🙈',
             },
         },
     },
